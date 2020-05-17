9a7eaa10e8095086453e347e81996b604cb8444c
Zissan Chowdhury <zissan.chowdhury@yahoo.com>
2020-05-17 17:29:04 +0530

    Component blocks

---
+++ src/pages/SearchBox/SearchBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Autocomplete from "lib/Autocomplete";
+import Button from "../../lib/Button";
+
+const SearchBox = () => {
+  return (
+    <div>
+      <Autocomplete />
+      <Button />
+    </div>
+  );
+};
+
+export default SearchBox;
---
+++ src/pages/SearchHistory/SearchHistory.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchHistory = () => {
+  return <div>Search History</div>;
+};
+
+export default SearchHistory;

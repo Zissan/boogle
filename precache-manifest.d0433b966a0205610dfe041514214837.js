cf3f49bd819c7182bcaa52c0cab4093cbee5adc1
Zissan Chowdhury <zissan.chowdhury@yahoo.com>
2020-05-25 08:25:11 +0530

    Updates

---
+++ precache-manifest.d0433b966a0205610dfe041514214837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13dca2d769c9f76acc326a4d4d4154e1",
+    "url": "/boogle/index.html"
+  },
+  {
+    "revision": "04e660ee2a1f09a560e0",
+    "url": "/boogle/static/css/main.35a3073e.chunk.css"
+  },
+  {
+    "revision": "2141d6b92b9dcd494741",
+    "url": "/boogle/static/js/2.7188b58a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boogle/static/js/2.7188b58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e660ee2a1f09a560e0",
+    "url": "/boogle/static/js/main.246738a1.chunk.js"
+  },
+  {
+    "revision": "932e2dbeb69d76c22bd3",
+    "url": "/boogle/static/js/runtime-main.5b30dd8b.js"
+  }
+]);
